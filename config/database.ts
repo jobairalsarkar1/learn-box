c0fb846a5b670683b0959864083fb43ec3da0140
Jobair Al Sarkar <jobairalsarkar1@gmail.com>
2025-10-05 11:33:05 +0600

    Database URL Configuration

--- config/database.ts
+++ config/database.ts
@@ -24,16 +24,8 @@ export default ({ env }) => {
     },
     postgres: {
       connection: {
-        // connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'postgres.railway.internal'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'railway'),
-        user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'PXNuqgmwrnknGbRDPSaRIVpwilBllDMa'),
-        ssl: env.bool('DATABASE_SSL', true) 
-          ? { rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false) } 
-          : false,
-        schema: env('DATABASE_SCHEMA', 'public'),
+        connectionString: env('DATABASE_URL'),
+        ssl: { rejectUnauthorized: false },
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
