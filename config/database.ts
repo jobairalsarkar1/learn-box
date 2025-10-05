055018ca0bf7be2951ff896fd1672ba79e69927d
Jobair Al Sarkar <jobairalsarkar1@gmail.com>
2025-10-05 12:39:54 +0600

    Neon Configuration

--- config/database.ts
+++ config/database.ts
@@ -25,7 +25,7 @@ export default ({ env }) => {
     postgres: {
       connection: {
         connectionString: env('DATABASE_URL'),
-        ssl: { rejectUnauthorized: false },
+        ssl: { require: true, rejectUnauthorized: false },
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
