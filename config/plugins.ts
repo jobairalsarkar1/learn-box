e9b5def944e3703e74342827894acd1911716c5c
Jobair Al Sarkar <jobairalsarkar1@gmail.com>
2025-10-05 13:01:36 +0600

    Mail: Mail Configuration added

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,21 @@
-export default () => ({});
+export default ({ env }) => ({
+  email: {
+    config: {
+      provider: 'smtp',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.gmail.com'),
+        port: env.int('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+        secure: false,
+        requireTLS: true,
+      },
+      settings: {
+        defaultFrom: env('SMTP_FROM'),
+        defaultReplyTo: env('SMTP_REPLY_TO'),
+      },
+    },
+  },
+});
